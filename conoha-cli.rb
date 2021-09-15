47bb528170b3bd8f2b3c093aefae7f3752e7ee0a
Ray81194 <49083285+Ray81194@users.noreply.github.com>
2021-09-15 21:55:30 +0900

    fix Formula

--- conoha-cli.rb
+++ conoha-cli.rb
@@ -1,5 +1,5 @@
 REPOSITORY_URL="https://github.com/ray81194/conoha-cli.git".freeze
-homebrew_gitpulls_version="0.0.1".freeze
+HOMEBREW_GITPULLS_VERSION="0.0.1".freeze
 class ConohaCli < Formula
   desc "Pulls all git repository under specified paths."
   homepage REPOSITORY_URL
