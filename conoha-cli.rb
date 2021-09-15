e1d12626405ceb2e93764ef57939231fcbd17b7a
Ray81194 <49083285+Ray81194@users.noreply.github.com>
2021-09-15 21:50:38 +0900

    fix Formula

--- conoha-cli.rb
+++ conoha-cli.rb
@@ -1,32 +1,17 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+REPOSITORY_URL="https://github.com/ray81194/conoha-cli.git".freeze
+homebrew_gitpulls_version="0.0.1".freeze
 class ConohaCli < Formula
-  desc ""
-  homepage ""
-  license ""
-  head "https://github.com/Ray81194/conoha-cli.git"
-
-  # depends_on "cmake" => :build
+  desc "Pulls all git repository under specified paths."
+  homepage REPOSITORY_URL
+  url "#{REPOSITORY_URL}/archive/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
+  sha256 "9b734ba7cad5d2e95a7ab003e03aec042844c1479398def3296ae3d9b89b145d"
+  head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_GITPULLS_VERSION
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "conoha"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test conoha-cli`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
