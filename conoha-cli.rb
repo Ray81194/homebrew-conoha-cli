13b245f6594c1fcab2a0d0576e8cc50e2a50e321
Ray81194 <49083285+Ray81194@users.noreply.github.com>
2021-09-15 21:59:49 +0900

    fix tar.gz

--- conoha-cli.rb
+++ conoha-cli.rb
@@ -3,7 +3,7 @@ HOMEBREW_GITPULLS_VERSION="0.0.1".freeze
 class ConohaCli < Formula
   desc "Pulls all git repository under specified paths."
   homepage REPOSITORY_URL
-  url "#{REPOSITORY_URL}/archive/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
+  url "#{REPOSITORY_URL}/archive/refs/tags/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
   sha256 "9b734ba7cad5d2e95a7ab003e03aec042844c1479398def3296ae3d9b89b145d"
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_GITPULLS_VERSION
 
