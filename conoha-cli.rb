bc3466ce861c990e8f4eea820f281416beaf5d2c
Ray81194 <49083285+Ray81194@users.noreply.github.com>
2021-09-26 10:12:50 +0900

    update 0.0.4

--- conoha-cli.rb
+++ conoha-cli.rb
@@ -1,10 +1,10 @@
 REPOSITORY_URL="https://github.com/ray81194/conoha-cli".freeze
-HOMEBREW_GITPULLS_VERSION="0.0.3".freeze
+HOMEBREW_GITPULLS_VERSION="0.0.4".freeze
 class ConohaCli < Formula
   desc "Pulls all git repository under specified paths."
   homepage REPOSITORY_URL
   url "#{REPOSITORY_URL}/archive/refs/tags/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
-  sha256 "272fb4992b7cf0e048d0e056ad4c8283ed7039f08395699788b57bf3407bbc1e"
+  sha256 "04bbebc7735938602e58026f8d1118569b9476bd2e6a760890cd01c79fc302a7"
 
   def install
     bin.install "conoha"
