bad6de2e40abfa26ccfac23f8c1c5c7ba25fe53e
Ray81194 <49083285+Ray81194@users.noreply.github.com>
2022-09-23 15:37:41 +0900

    update 0.0.5

--- conoha-cli.rb
+++ conoha-cli.rb
@@ -4,7 +4,7 @@ class ConohaCli < Formula
   desc "Pulls all git repository under specified paths."
   homepage REPOSITORY_URL
   url "#{REPOSITORY_URL}/archive/refs/tags/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "1550119ded6b541d5439c12475e382bb61d4b822009918797f07d78009186a41"
 
   def install
     bin.install "conoha"
